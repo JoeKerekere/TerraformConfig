79003b645b4f3c7dbdc67215633b273dceddb582
Víctor Cobo <149813236+JoeKerekere@users.noreply.github.com>
2023-11-13 16:50:34 +0100

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#installations
+apt-get update
+apt-get install apache2 -y
+
+#virtualhost
+
+cp /etc/backups/grup8vc2324.conf /etc/apache2/sites-available
+a2dissite 000-default.conf
+a2ensite grup8vc2324.conf
+
+#digest-autentication
+
+a2enmod auth_digest
+cp /etc/backups/passwords.digest /etc/apache2
+chmod 640 /etc/apache2/passwords.digest
+chown root.www-data /etc/apache2/passwords.digest
+
+#basic-autentication
+
+cp /etc/backups/ba-pw /etc/apache2
+
+#webalizer
+
+cp /etc/backups/webalizer.conf /etc/webalizer
+cp /etc/backups/webalizer-vh.conf /etc/apache2/sites-available
+sudo a2ensite webalizer-vh.conf
+mkdir /usr/share/GeoDB
+cp /etc/backups/GeoDB.dat /usr/share/GeoDB
+crontab crontab_webalizer
+(crontab -l; echo "* * * * * webalizer")|awk '!x[$0]++'|crontab -
+
+#server-info
+
+a2enmod info
+cp /etc/backups/info.conf /etc/apache2/mods-available
+
+#server-status
+
+a2enmod status
+cp /etc/backups/status.conf /etc/apache2/mods-available
+
+#restart apache
+
+systemctl restart apache2
