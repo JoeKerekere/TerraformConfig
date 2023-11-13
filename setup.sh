503ca418a4ca7054d6a506f22157ef05c78766c0
VictorCoboCopernic <cobo.jandula.victor@alumnat.copernic.cat>
2023-11-13 19:19:22 +0100

    setup created in ubuntu

--- setup.sh
+++ setup.sh
@@ -1,47 +1,47 @@
 #!/bin/bash
 
 #installations
-sudo apt-get update
-sudo apt-get install apache2 -y
+apt-get update
+apt-get install apache2 -y
 
 #virtualhost
 
-sudo cp -r /etc/backups/grup8vc2324-html /var/www/grup8vc2324/
-sudo cp /etc/backups/grup8vc2324.conf /etc/apache2/sites-available
-sudo a2dissite 000-default.conf
-sudo a2ensite grup8vc2324.conf
+cp -r /etc/backups/grup8vc2324-html /var/www/grup8vc2324/
+cp /etc/backups/grup8vc2324.conf /etc/apache2/sites-available
+a2dissite 000-default.conf
+a2ensite grup8vc2324.conf
 
 #digest-autentication
 
-sudo a2enmod auth_digest
-sudo cp /etc/backups/passwords.digest /etc/apache2
-sudo chmod 640 /etc/apache2/passwords.digest
-sudo chown root.www-data /etc/apache2/passwords.digest
+a2enmod auth_digest
+cp /etc/backups/passwords.digest /etc/apache2
+chmod 640 /etc/apache2/passwords.digest
+chown root.www-data /etc/apache2/passwords.digest
 
 #basic-autentication
 
-sudo cp /etc/backups/ba-pw /etc/apache2
+cp /etc/backups/ba-pw /etc/apache2
 
 #webalizer
 
-sudo cp /etc/backups/webalizer.conf /etc/webalizer
-sudo cp /etc/backups/webalizer-vh.conf /etc/apache2/sites-available
+cp /etc/backups/webalizer.conf /etc/webalizer
+cp /etc/backups/webalizer-vh.conf /etc/apache2/sites-available
 sudo a2ensite webalizer-vh.conf
-sudo mkdir /usr/share/GeoDB
-sudo cp /etc/backups/GeoDB.dat /usr/share/GeoDB
-sudo crontab crontab_webalizer
-sudo (crontab -l; echo "* * * * * webalizer")|awk '!x[$0]++'|crontab -
+mkdir /usr/share/GeoDB
+cp /etc/backups/GeoDB.dat /usr/share/GeoDB
+crontab crontab_webalizer
+(crontab -l; echo "* * * * * webalizer")|awk '!x[$0]++'|crontab -
 
 #server-info
 
-sudo a2enmod info
-sudo cp /etc/backups/info.conf /etc/apache2/mods-available
+a2enmod info
+cp /etc/backups/info.conf /etc/apache2/mods-available
 
 #server-status
 
-sudo a2enmod status
-sudo cp /etc/backups/status.conf /etc/apache2/mods-available
+a2enmod status
+cp /etc/backups/status.conf /etc/apache2/mods-available
 
 #restart apache
 
-sudo systemctl restart apache2
+systemctl restart apache2
